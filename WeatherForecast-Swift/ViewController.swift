c075cc3dc2b6fb10a4e35d60b0f28e63f7752571
Igor Golubovich <igar.galubovich@gmail.com>
2017-04-10 09:25:59 +0300

    Initial commit

---
+++ WeatherForecast-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherForecast-Swift
+//
+//  Created by Igor Golubovich on 4/10/17.
+//  Copyright © 2017 IG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
